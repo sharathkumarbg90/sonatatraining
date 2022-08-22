722cb4765fc54dbc2852ceb42ba0e8bbcb34d860
sharath <sharath.bg@sonata-software.com>
2022-08-22 19:04:40 +0530

    Initial version

---
+++ src/main/java/com/example/demo/SonatatrainingApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.impl.DefaultAddition;
+
+@SpringBootApplication
+public class SonatatrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonatatrainingApplication.class, args);
+		AddInterface addition = new DefaultAddition();
+		System.out.println(addition.add(10,20));
+	}
+
+}
