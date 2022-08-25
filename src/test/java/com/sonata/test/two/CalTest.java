028c48dc9a6719e9657130f552c2a577e459ca07
sharath <sharath.bg@sonata-software.com>
2022-08-25 14:20:20 +0530

    new

--- src/test/java/com/sonata/test/two/CalTest.java
+++ src/test/java/com/sonata/test/two/CalTest.java
@@ -8,22 +8,22 @@ import com.sonata.two.Calculator;
 
 
 
-public class CalTest {
+ class CalTest {
 	Calculator c3 = new Calculator();
 
 		@Test
-		public void addTest() {
+		 void addTest() {
 			Calculator c1 = new Calculator();
 			Assertions.assertEquals(4, c1.add(2,2));
 		}
 			@Test
-			public void mulTest() {
+			 void mulTest() {
 				Calculator c2 = new Calculator();
 				Assertions.assertEquals(4, c2.mul(2,2));
 		}
 		
 			@Test
-			public void divTest() {
+			 void divTest() {
 				int x = c3.div(1, 1);
 			
 				assertThrows(ArithmeticException.class,()->c3.div(1,0),"it is an error");
